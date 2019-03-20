00b3c0384984f23c6ebbd47c5d2d0e342d5db522
Urth <dragondor@protonmail.com>
2019-03-20 11:51:05 +0100

    Creation fichierwilders, gitignore _modules

---
+++ wilders.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+const collegues =['Jason', 'Paulo', 'Name 3', 'Name 4'];
+
+console.log(chalk.blue(collegues [0]) + ' ' + chalk.red(collegues [1]) + ' ' + chalk.green(collegues [2]) + ' ' + chalk.black(collegues [3]))
